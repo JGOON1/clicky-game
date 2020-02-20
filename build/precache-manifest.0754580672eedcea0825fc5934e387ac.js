efa0b0710c4cbb40693e5330a7d6b37cda778995
john Meng <john.meng014@gmail.com>
2020-02-20 12:18:12 -0500

    reformatting

---
+++ build/precache-manifest.0754580672eedcea0825fc5934e387ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63383e25d3bd8775f04e27a8d5841b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c693b907ecea3532b7",
+    "url": "/static/css/main.619c8c13.chunk.css"
+  },
+  {
+    "revision": "4e231b10d16ec6c0094d",
+    "url": "/static/js/2.ce41a8b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ce41a8b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85c693b907ecea3532b7",
+    "url": "/static/js/main.da385511.chunk.js"
+  },
+  {
+    "revision": "10eb1490ca643447ff29",
+    "url": "/static/js/runtime-main.827bc4e2.js"
+  }
+]);
